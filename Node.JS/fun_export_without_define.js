65983c82e10c578e48a1aa8c97c5131e97ee5398
SIDDHARTH-101251 <singhsiddharth101251@gmail.com>
2024-02-07 21:45:04 +0530

    Create fun_export_without_define.js file

---
+++ Node.JS/fun_export_without_define.js
@@ -0,0 +1,5 @@
+//we can directly export function while defining
+
+module.exports = function add(num1, num2) {
+    return num1 + num2;
+}
--- Node.JS/index.js
+++ Node.JS/index.js
@@ -1,5 +1,7 @@
 const variable = require("./var_export");
 const expression = require("./expr_export");
+const add = require("./fun_export_without_define");
 
 console.log(variable);
 console.log(expression);
+console.log(add(7, 4));
