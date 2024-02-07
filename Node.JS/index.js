413bdbdca9c4772e1793d971d9467dc0d1fc28be
SIDDHARTH-101251 <singhsiddharth101251@gmail.com>
2024-02-07 22:46:20 +0530

    Added class_export_after_defining.js

--- Node.JS/index.js
+++ Node.JS/index.js
@@ -4,6 +4,7 @@ const add = require("./fun_export_without_define");
 const sub = require("./fun_export_after_define");
 const student = require("./class_export_while_defining");
 let StudentDetails = require("./class_export_while_defining");
+let Car = require("./class_export_after_defining");
 
 console.log("variable : " ,variable);
 console.log("Expression : ", expression);
@@ -11,5 +12,8 @@ console.log("add : ", add(7, 4));
 console.log("sub : ", sub(4, 11));
 
 StudentDetails = new StudentDetails("Siddharth", 21052029);
-
 console.log(StudentDetails);
+
+Car = new Car("Audi", 10000000);
+console.log(Car);
+
