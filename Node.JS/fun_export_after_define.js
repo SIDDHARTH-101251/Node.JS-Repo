7d4b5ad8032f2c3f7b255846dbfe682071191170
SIDDHARTH-101251 <singhsiddharth101251@gmail.com>
2024-02-07 21:49:27 +0530

    Added fun_export_after_define.js file

---
+++ Node.JS/fun_export_after_define.js
@@ -0,0 +1,7 @@
+//we can export function after definition
+
+function sub(num1, num2) {
+    return num2 - num1;
+}
+
+module.exports = sub;
--- Node.JS/index.js
+++ Node.JS/index.js
@@ -1,7 +1,9 @@
 const variable = require("./var_export");
 const expression = require("./expr_export");
 const add = require("./fun_export_without_define");
+const sub = require("./fun_export_after_define");
 
-console.log(variable);
-console.log(expression);
-console.log(add(7, 4));
+console.log("variable : " ,variable);
+console.log("Expression : ", expression);
+console.log("add : ", add(7, 4));
+console.log("sub : ", sub(4, 11));
