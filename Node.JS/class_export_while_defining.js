95d312cc010d6c2aef295ad53b860e6e74efeee5
SIDDHARTH-101251 <singhsiddharth101251@gmail.com>
2024-02-07 21:57:43 +0530

    Added class_export_while_defining.js file

---
+++ Node.JS/class_export_while_defining.js
@@ -0,0 +1,9 @@
+//we can export class while defining it
+
+module.exports = class StudentDetails {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+};
+
--- Node.JS/index.js
+++ Node.JS/index.js
@@ -2,8 +2,14 @@ const variable = require("./var_export");
 const expression = require("./expr_export");
 const add = require("./fun_export_without_define");
 const sub = require("./fun_export_after_define");
+const student = require("./class_export_while_defining");
+let StudentDetails = require("./class_export_while_defining");
 
 console.log("variable : " ,variable);
 console.log("Expression : ", expression);
 console.log("add : ", add(7, 4));
 console.log("sub : ", sub(4, 11));
+
+StudentDetails = new StudentDetails("Siddharth", 21052029);
+
+console.log(StudentDetails);
