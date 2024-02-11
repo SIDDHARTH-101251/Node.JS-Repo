c34651b4b3e66321d32abebc5e1f19192e01ec35
SIDDHARTH-101251 <singhsiddharth101251@gmail.com>
2024-02-11 10:48:07 +0530

    Used date-fns module and printed a date

--- NPM/index.js
+++ NPM/index.js
@@ -3,3 +3,9 @@ const path = require("path");
 const filePath = path.join("users", "ravi", "notes.txt");
 
 console.log(filePath);
+
+const { addDays } = require("date-fns/addDays");
+
+const result = addDays(new Date(2021, 0, 11), 10);
+
+console.log(result);
